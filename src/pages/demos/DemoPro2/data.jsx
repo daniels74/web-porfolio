6c8ebcd13305c824db8e2f1eb23fbb310467f3e4
fadeddiamond74 <daniel.hurtado4774@gmail.com>
2022-06-01 17:22:16 -0500

    pro2-description-added

--- src/pages/demos/DemoPro2/data.jsx
+++ src/pages/demos/DemoPro2/data.jsx
@@ -9,31 +9,31 @@ const data = [
         id:2,
         img: "Capture1.PNG",
         img0: "pro2",
-        description: "Again, use the make command, but this time to call the runn part of the makefile."
+        description: "Typing chcp 65001 will activate icon abilities on windows."
     },
     {
         id:3,
         img: "Capture2.PNG",
         img0: "pro2",
-        description: "Type a number."
+        description: "Again, use the make command, but this time to call the runn part of the makefile."
     },
     {
         id:4,
         img: "Capture3.PNG",
         img0: "pro2",
-        description: "Answer the questions using numbers only."
+        description: "Type in the file name, cities.txt is the longest. "
     },
     {
         id:5,
         img: "Capture4.PNG",
         img0: "pro2",
-        description: "Choose a grouping of characters. Press enter and obtain result."
+        description: "Choose your time frame by typing 1 or type 2 for the full timelapse. "
     },
     {
         id:6,
         img: "Capture5.PNG",
         img0: "pro2",
-        description: "Choose a grouping of characters. Press enter and obtain result."
+        description: "You will be prompted to type the time intervals if you chose 1, if not, the chart should start changing."
     }
 
 ]
