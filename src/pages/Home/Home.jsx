c1901e7f655db0e0e29d5927d18605751149b3fb
fadeddiamond74 <daniel.hurtado4774@gmail.com>
2022-05-31 23:59:13 -0500

    homepage-fix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,29 +1,50 @@
-import React from 'react'
-import "./Home.css"
-import Footer from "../../components/Footer/Footer"
+import React from "react";
+import "./Home.css";
+import Footer from "../../components/Footer/Footer";
+// import { Holdable } from "react-touch";
+// import { defineHold } from "react-touch";
 
 const Home = () => {
+  const [btn1, setBtn1] = React.useState(false);
+
+  function togglebtn1() {
+    return setBtn1(!btn1);
+  }
+
+  const [btn2, setBtn2] = React.useState(false);
+
+  function togglebtn2() {
+    return setBtn2(!btn2);
+  }
   return (
-    <div className="main">
-        <div className="home">
-            <h2>Hello and Welcome Employer</h2>
-            <h1>This is my Porfolio</h1>
-            <p>You can view my projects by 
-              clicking the projects button, or you can learn about me in
-              general by clicking about.
-            </p>
+    <div className="home-container">
+      <div className="home">
+        <h2>Hello and Welcome Employer</h2>
+        <h1>This is my Porfolio</h1>
+        <p>
+          You can view my projects by clicking the projects button, or you can
+          learn about me in general by clicking about.
+        </p>
         <div className="home_buttons">
           <a href="/Projects">
-            <button className="home_btn-Projects">Projects</button>
+          <button
+              className={btn1 ? "home_btns-active" : "home_btns"}
+              onTouchStart={togglebtn1}
+            >Projects</button>
           </a>
           <a href="/About">
-            <button className="home_btn-About">About</button>
+            <button
+              className={btn2 ? "home_btns-active" : "home_btns"}
+              onTouchStart={togglebtn2}
+            >
+              About
+            </button>
           </a>
         </div>
-        </div>
-        <Footer />
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
