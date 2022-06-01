3a4119ae672a741ef12b7fb25d9b23853cb570e5
fadeddiamond74 <daniel.hurtado4774@gmail.com>
2022-06-01 17:33:59 -0500

    labyrnith-update

--- src/pages/demos/DemoPro3/data.jsx
+++ src/pages/demos/DemoPro3/data.jsx
@@ -2,38 +2,32 @@ const data = [
     {
         id: 1,
         img: "Capture.PNG",
-        img0: "pro2",
-        description: "Use the make command to call upon the g++ builder, this will make our executable. make buildd"
+        img0: "pro3",
+        description: "make buildd"
     },
     {
         id:2,
         img: "Capture1.PNG",
-        img0: "pro2",
-        description: "Again, use the make command, but this time to call the runn part of the makefile."
+        img0: "pro3",
+        description: "make run_tests"
     },
     {
         id:3,
         img: "Capture2.PNG",
-        img0: "pro2",
-        description: "Type a number."
+        img0: "pro3",
+        description: "make build_reg"
     },
     {
         id:4,
         img: "Capture3.PNG",
-        img0: "pro2",
-        description: "Answer the questions using numbers only."
+        img0: "pro3",
+        description: "make run_reg"
     },
     {
         id:5,
         img: "Capture4.PNG",
-        img0: "pro2",
-        description: "Choose a grouping of characters. Press enter and obtain result."
-    },
-    {
-        id:6,
-        img: "Capture5.PNG",
-        img0: "pro2",
-        description: "Choose a grouping of characters. Press enter and obtain result."
+        img0: "pro3",
+        description: "Inputting y will print the node path. "
     }
 
 ]
