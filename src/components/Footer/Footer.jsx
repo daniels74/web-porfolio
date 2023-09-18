a55959e05ff3a3099028f02d69caa7bc83664960
Daniel Flores Hurtado <daniel.hurtado4774@gmail.com>
2023-09-18 15:18:50 -0500

    Upgraded footer bar toresponsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,50 +1,100 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Footer.css"
+import Box from '@mui/material/Box';
 // import {FaLinkedin} from "react-icons/fa"
 // import {SiHandshake} from "react-icons/si"
 
+
 const Footer = () => {
-  return (
-    <footer className="footer">
-        <div className="leftside">
-            {/* <h4>Socials</h4>
-            <ul className='list'>
-                <li>
-                    <a href="https://app.joinhandshake.com/stu/users/26034444">
-                       <SiHandshake/>
-                        Handshake
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.linkedin.com/in/daniel-flores-hurtado-880396149/">
-                       <FaLinkedin/>
-                        LinkedIn
-                    </a>
-                </li>
-            </ul> */}
-        </div>
-        <div className="rightside">
-            <h4>Personal Sites in Progress</h4>
-            <ul className="list">
-                <li>
-                    <a href="https://cryptonica.vercel.app/">
-                        Cryptonika
-                    </a>
-                </li>
-                <li>
-                    <a href="https://royerslandscaping.vercel.app/">
-                        LandPros
-                    </a>
-                </li>
-                <li>
-                    <a href="https://thelandscapes.vercel.app/">
-                        Landscapes
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </footer>
-  )
+    const [footerState, setFooterState] = useState(false);
+
+    return (
+        <Box onMouseEnter={() => setFooterState(true)} onMouseLeave={() => setFooterState(false)} component="footer" sx={{
+            height: footerState ? "20%" : "10%",
+            width: footerState ? "100%" : "25%",
+            backgroundColor: "rgb(8, 20, 33)",
+            color: "lightblue",
+            position: "fixed",
+            bottom: 0,
+            borderRadius: "1rem",
+            borderStyle: "solid",
+            borderWidth: "thin",
+            borderColor: "lightBlue",
+        }}>
+            <Box sx={{
+                height: "100%",
+                width: "100%",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "space-around",
+            }}>
+                <Box component="h4" sx={{
+                    height: "30%",
+                    // background: "red",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}>Personal Sites in Progress</Box>
+                <Box sx={{
+                    height: "75%",
+                    width: "100%",
+                    listStyle: "none",
+                    display: footerState ? "flex" : "none",
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "space-around",
+                }}>
+                    <Box component="li" sx={{
+                        display: footerState ? "flex" : "none",
+                    }}>
+                        <Box component='a' sx={{
+                            color: "lightblue"
+                        }} href="https://cold-jupiter-d4aac7c2f1f0.herokuapp.com/user/28">
+                            CryptoZapp
+                        </Box>
+                    </Box>
+                    <Box component="li" sx={{
+                        display: footerState ? "flex" : "none",
+                    }}>
+                        <Box component='a' sx={{
+                            color: "lightblue"
+                        }} href="https://wiseupv3.herokuapp.com/">
+                            WiseUp
+                        </Box>
+                    </Box>
+                    <Box component="li" sx={{
+                        display: footerState ? "flex" : "none",
+                    }}>
+                        <Box component='a' sx={{
+                            color: "lightblue"
+                        }} href="https://cryptonica.vercel.app/">
+                            Cryptonika
+                        </Box>
+                    </Box>
+                    <Box component="li" sx={{
+                        display: footerState ? "flex" : "none",
+                    }}>
+                        <Box component='a' sx={{
+                            color: "lightblue"
+                        }} href="https://royerslandscaping.vercel.app/">
+                            LandPros
+                        </Box>
+                    </Box>
+                    <Box component="li" sx={{
+                        display: footerState ? "flex" : "none",
+                    }}>
+                        <Box component='a' sx={{
+                            color: "lightblue"
+                        }} href="https://thelandscapes.vercel.app/">
+                            Landscapes
+                        </Box>
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
+    )
 }
 
 export default Footer
